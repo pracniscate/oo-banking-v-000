75930a8bccec1b23266e700b5093728020ee324d
Lorena <lorena.giddens@outlook.com>
2018-06-17 13:45:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,4 +25,12 @@ class Transfer
       "Transaction rejected. Please check your account balance."
     end
   end
+
+  def reverse_transfer
+    if @status = "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
 end
