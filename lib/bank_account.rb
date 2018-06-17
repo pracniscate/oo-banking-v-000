de7d701c3c40dc55d4057ef780ca852aa15f70df
Lorena <lorena.giddens@outlook.com>
2018-06-17 13:23:08 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,8 @@
 class BankAccount
-  attr_accessor :bank_account
+  attr_accessor :bank_account, :name
 
-  def initialize(bank_account)
+  def initialize(bank_account, name)
     @bank_account = bank_account
+    @name = name
   end
 end
