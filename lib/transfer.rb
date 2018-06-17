1fc694e533818b443c719443d8a0df0c98151c7c
Lorena <lorena.giddens@outlook.com>
2018-06-17 13:41:33 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,6 +11,16 @@ class Transfer
 
   def valid?
     # check that both accounts are valid
+    # call on the sender & receiver's #valid? methods
     @sender.valid? && @receiver.valid? && @sender.balance > @amount
+  end
+
+  def execute_transaction
+    if self.valid? && stauts == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount 
+      @status = "complete"
+    else
+      @status = "rejected"
   end
 end
